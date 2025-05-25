831f69546993dd1a440f6528b9ff5bae6aafbc06
Danny016 <danydango001@gmail.com>
2025-05-25 00:34:41 -0600

    Pantalla de Inicio (Home)

---
+++ app/src/main/java/com/example/caritas/ui/theme/NavGraph.kt
@@ -0,0 +1,18 @@
+package com.example.caritas.ui.theme
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.caritas.Screens.HomeScreen
+
+@Composable
+fun Nav(){
+    val NavController = rememberNavController()
+    NavHost(navController = NavController, startDestination = "Home"){
+        composable(route = "Home") {
+            HomeScreen(navController = NavController)
+        }
+    }
+}
